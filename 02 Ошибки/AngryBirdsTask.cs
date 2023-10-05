e0f722772c5ee47320e2d48e804843d6c5c41238
Юрий Петриков <146861042+YuriiPetrikov@users.noreply.github.com>
2023-10-05 13:46:56 +0700

    Update AngryBirdsTask.cs

--- 02 Ошибки/AngryBirdsTask.cs
+++ 02 Ошибки/AngryBirdsTask.cs
@@ -1,1 +1,20 @@
+// Вставьте сюда финальное содержимое файла AngryBirdsTask.cs
+using System;
 
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		// Ниже — это XML документация, её использует ваша среда разработки, 
+		// чтобы показывать подсказки по использованию методов. 
+		// Но писать её естественно не обязательно.
+		/// <param name="v">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public static double FindSightAngle(double v, double distance)
+		{
+			//return Math.PI / 4;
+			return 0.5 * Math.Asin(distance*9.8 / (v*v));
+    }
+	}
+}
