288387b60e06f49529cc4df29d70ed937a4ca75c
Юрий Петриков <146861042+YuriiPetrikov@users.noreply.github.com>
2023-10-06 09:05:44 +0700

    Update VectorTask.cs

--- 12 Основы ООП/VectorTask.cs
+++ 12 Основы ООП/VectorTask.cs
@@ -3,16 +3,46 @@ using System;
 
 namespace GeometryTasks
 {
+    //Поэтому вы решили сохранить этот класс, но добавить методы
+    //Vector.GetLength(), +++
+    //Segment.GetLength() +++,
+    //Vector.Add(Vector),++
+    //Vector.Belongs(Segment),+++
+    //Segment.Contains(Vector)
     public class Segment
     {
         public Vector Begin;
         public Vector End;
+
+        public bool Contains(Vector a)
+        {
+            return Geometry.IsVectorInSegment(a, this);
+        }
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
     }
 
     public class Vector
     {
         public double X;
         public double Y;
+        public bool Belongs(Segment a)
+        {
+            return Geometry.IsVectorInSegment(this, a);
+        }
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector b)
+        {
+            return new Vector() { X = this.X + b.X, Y = this.Y + b.Y };
+        }
     }
 
     public class Geometry
